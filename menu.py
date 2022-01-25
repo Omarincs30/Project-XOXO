324ff7baed1bdea4019bd74a2ab50763ad2dc1dd
omarincs30 <90853239+Omarincs30@users.noreply.github.com>
2022-01-25 06:49:58 +0000

    Fixed the Menu

--- menu.py
+++ menu.py
@@ -3,7 +3,7 @@
 # January 25, 2021
 # FINAL Project (Menu File)
 
-
+# Here I imported Pygame.
 import pygame, sys
 
 mainClock = pygame. time.Clock()
@@ -14,21 +14,20 @@ screen = pygame.display.set_mode((600, 600), 0, 100)
 
 font = pygame. font. SysFont(None, 30)
 
+
 def draw_text(text, font, color, surface, x, y):
     textobj = font.render(text, 2, color)
     textrect = textobj.get_rect()
     textrect.topleft = (x, y)
     surface.blit(textobj, textrect)
 
-#click = False
 
 def main_menu():
     while True:
 
-        screen.fill((0,0,0,))
+        screen.fill((0, 0, 0))
         draw_text('MAIN MENU', font, (255, 255, 255), screen, 235, 25)
         draw_text('To start playing, click the purple rectrangle below.', font, (255, 255, 255), screen, 50, 70)
-
         mx, my = pygame.mouse.get_pos()
 
         button_1 = pygame.Rect(150, 150, 300, 50)
@@ -47,13 +46,13 @@ def main_menu():
                 pygame.quit()
                 sys.exit()
             if event.type == MOUSEBUTTONDOWN:
-                if event.button== 1:
+                if event.button == 1:
                     click = True
 
         pygame.display.update()
         mainClock.tick(60)
-        
 
+# Here is the button function and were the game is imported.
 def game():
     running = True
     while running:
@@ -67,7 +66,6 @@ def game():
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     running = False
-        
         pygame.display.update()
         mainClock.tick(60)
 
